f995ccbab56b09240ea75be1023a5e3d8e7a39ed
Marcel Assis <marcel@marcelassis.com>
2019-06-27 21:32:17 -0300

    Search profile by id

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+        await mongoose.connect(db, 
+            { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
         console.log('MongoDb Connected...');
 
     } catch(err) {
