19f2b123cd0fcd7f2c94980ddd6b5291a1b5f94a
Marcel Assis <marcel@marcelassis.com>
2019-07-02 10:15:25 -0300

    Adding logout function

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 
 import setAuthToken from '../utils/setAuthtoken';
@@ -91,4 +92,11 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+// logout and clear / profile
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  });
 };
